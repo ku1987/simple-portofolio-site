8ea97f61624441ca44498bfbb9787751a7a1959d
ku1987 <kei.usami1987@gmail.com>
2023-07-31 21:53:36 +0900

    Add footer

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="fixed h-8 bottom-4 right-2/4 translate-x-1/2 text-xs">
+      <div>
+        <p>Copyright © 2023 Kei Usami</p>
+      </div>
+    </footer>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Header from "./components/header";
+import Footer from "./components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,6 +26,7 @@ export default function RootLayout({
         >
           {children}
         </div>
+        <Footer />
       </body>
     </html>
   );
