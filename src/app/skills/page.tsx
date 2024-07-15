1a2a8f5dbd59199f8a3b9c9a1c862b25d2da70a8
ku1987 <kei.usami1987@gmail.com>
2024-07-15 18:06:29 +0900

    feat: update skills

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -8,13 +8,17 @@ export default function Skills() {
       <div className="flex flex-col items-center justify-between">
         <h1>Skills</h1>
       </div>
+      <h2>Team Lead</h2>
+      <h2>Project Management</h2>
+      <h2>System Architecture Design</h2>
+      <h2>Monitoring, Operations</h2>
       <h2>Backend</h2>
       <ul>
         <li>Python</li>
         <li>Django</li>
+        <li>Go</li>
         <li>Node.js</li>
         <li>Express</li>
-        <li>Koa.js</li>
       </ul>
       <h2>Infrastructure, Cloud architecture</h2>
       <ul>
@@ -28,8 +32,14 @@ export default function Skills() {
         <li>TypeScript</li>
         <li>React</li>
         <li>Next.js</li>
-        <li>Vue.js</li>
-        <li>Nuxt.js</li>
+      </ul>
+      <h2>Other</h2>
+      <ul>
+        <li>Git</li>
+        <li>CI/CD</li>
+        <li>DevOps</li>
+        <li>OAuth2.0</li>
+        <li>OpenID Connect, SSO</li>
       </ul>
     </main>
   );
